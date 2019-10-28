2ef9ffdfe56b321a06d9e4a3b62f08f85a606cd1
Pierre-Olivier Trottier <po.trottier@gmail,com>
2019-10-28 14:35:52 -0400

    Setup Main Class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
-
     public static void main(String[] args) {
-	    System.out.println("Hello");
+	    int[] arr1 = {1,2,3,4,5,0};
+	    play(arr1, 0);
+    }
+
+    private static void play(int[] array, int position) {
+        System.out.println(array.length);
     }
 }
+
