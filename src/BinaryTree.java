fb23be163bbb2731ed2a5fd57d412e1060be5e39
NimitJaggi <nimit.jaggi@hotmail.ca>
2019-10-30 01:41:58 -0400

    Create BinaryTree.java

---
+++ src/BinaryTree.java
@@ -0,0 +1,53 @@
+public class BinaryTree {
+
+    private static Node root;
+
+    public BinaryTree(int data)
+    {
+        root = new Node(data);
+    }
+
+    public void add(Node parent, Node child, String side)
+    {
+        if (side.equals("left"))
+        {
+            parent.setLeft(child);
+        }
+        else
+        {
+            parent.setRight(child);
+        }
+    }
+
+
+    class Node {
+        private int key;
+        private Node left;
+        private Node right;
+
+        Node(int key) {
+            this.key = key;
+            right = null;
+            left = null;
+        }
+
+
+
+        public void setLeft(Node left) {
+            this.left = left;
+        }
+
+        public Node getLeft() {
+            return left;
+        }
+
+        public void setRight(Node right) {
+            this.right = right;
+        }
+
+        public Node getRight() {
+            return right;
+        }
+    }
+
+}
